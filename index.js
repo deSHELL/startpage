f74db9caa206abc99d76cc6acba3b57b2d7b420c
Deva Midhun <admin@coderrrrr.site>
2024-09-30 17:44:28 +0530

    update!

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-elemHour = document.getElementById("hour");
-elemMin = document.getElementById("min");
-elemApm = document.getElementById("apm");
-elemSearch = document.getElementById("search");
+const duration = 1.7;
+const from = { opacity: 0, ease: Linear.ease };
+const to = { opacity: 1 };
+const time = document.getElementsByClassName("time");
 
-elemHour.innerHTML = localStorage['hour'];
-elemMin.innerHTML = localStorage['min'];
-elemApm.innerHTML = localStorage['apm'];
+function fadeIn() {
+   TweenLite.fromTo(time, duration, from, to);
+}
 
-setInterval(() => {
-  let time = new Date();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let apm = hour > 11 ? "PM" : "AM";
-
-  hour = hour > 12 ? hour - 12 : hour;
-  hour = hour < 10 ? "0" + hour.toString() : hour.toString();
-  min = min < 10 ? "0" + min.toString() : min.toString();
-
-  elemHour.innerHTML = hour;
-  elemMin.innerHTML = min;
-  elemApm.innerHTML = apm;
-  localStorage['hour'] = hour;
-  localStorage['min'] = min;
-  localStorage['apm'] = apm;
-}, 1000);
-
-elemSearch.addEventListener("keydown", (event) => {
-    if (event.key === 'Enter') {
-        window.location.href = `https://www.duckduckgo.com/?q=${elemSearch.value}`;
-    }
-})
+document.addEventListener("DOMContentLoaded", (event) => {
+	fadeIn();
+});
